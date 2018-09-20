4fdd541b2d5a464698a38f24468dd8509dc1d427
Sophia Rodriguez-Bell <sophiarodriguezbell@students.berkeley.net>
2018-09-20 13:59:12 -0700

    starting

--- script.js
+++ script.js
@@ -16,6 +16,36 @@ function monkey_trouble(a_smile, b_smile){
     }
 }
 
+// This is the third problem, string_times.
+function string_times(string, n){
+    var newString = "";
+    for(var i = 0; i < n; i++){
+        newString += string;
+    }
+    return newString;
+}
+
+
+// This is the fourth problem, front_times.
+function front_times(string, n){
+    var str = string.substring(0, 3);
+    var newString = "";
+    for (var i = 0; i < n; i++){
+        newString += str;
+    }
+    return newString;
+}
+
+// This is the fifth problem, string_bits.
+function string_bits(string){
+    var stringLength = string.length;
+    var newString = "";
+    for (var i = 0; i < stringLength; i +=2){
+        newString += string.charAt(i);
+    }
+    return newString;
+}
+
 //write second method
 function nextOne(param1, param2) {
     return param1;
